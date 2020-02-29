b4a6a1a2e3abeec50d8b40a0cd512523cae3010b
VsevolodKamenskiy <47222000+VsevolodKamenskiy@users.noreply.github.com>
2020-03-01 00:11:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                var x = new XORCipher();
+                Console.Write("Введите текст сообщения: ");
+                var message = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                var pass = Console.ReadLine();
+                var encryptedMessageByPass = x.Encrypt(message, pass);
+                Console.WriteLine("Зашифрованное сообщение {0}", encryptedMessageByPass);
+                Console.WriteLine("Расшифрованное сообщение {0}", x.Decrypt(encryptedMessageByPass, pass));
+                Console.ReadLine();
+            
+        }
+    }
+}
---
+++ XORCipher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class XORCipher
+    {
+        //генератор повторений пароля
+        private string GetRepeatKey(string s, int n)
+        {
+            var r = s;
+            while (r.Length < n)
+            {
+                r += r;
+            }
+
+            return r.Substring(0, n);
+        }
+
+        //метод шифрования/дешифровки
+        private string Cipher(string text, string secretKey)
+        {
+            var currentKey = GetRepeatKey(secretKey, text.Length);
+            var res = string.Empty;
+            for (var i = 0; i < text.Length; i++)
+            {
+                res += ((char)(text[i] ^ currentKey[i])).ToString();
+            }
+
+            return res;
+        }
+
+        //шифрование текста
+        public string Encrypt(string plainText, string password)
+        {
+            return Cipher(plainText, password);
+        }
+
+
+        //расшифровка текста
+        public string Decrypt(string encryptedText, string password)
+        {
+            return Cipher(encryptedText, password);
+        }
+    }
+}
